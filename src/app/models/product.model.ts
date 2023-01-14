37ec85b012e6b01cff8009e31b73107bc9a192fb
AndersonGG21 <andersongarcesgarcia@gmail.com>
2023-01-14 18:18:32 -0500

    Update product model

--- src/app/models/product.model.ts
+++ src/app/models/product.model.ts
@@ -3,12 +3,14 @@ export class Product{
   public name:string = '';
   public urlImg:string = '';
   public price:number = 0;
-  public category:string = ''
+  public category:string = '';
+  public quantity: number = 0;
 
-  constructor(name: string, imgUrl: string, price: number, category: string){
+  constructor(name: string, imgUrl: string, price: number, category: string, quantity: number){
     this.name = name;
     this.urlImg = imgUrl;
     this.price = price;
     this.category = category;
+    this.quantity = quantity;
   }
 }
